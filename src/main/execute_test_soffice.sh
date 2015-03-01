b1f7dc8be5d5e588453fe5a66997b34c1d5af971
Bogdan Apetrei <bogdanapetrei85@yahoo.com>
2015-03-01 12:00:28 +0200

    almost done I think

--- src/main/execute_test_soffice.sh
+++ src/main/execute_test_soffice.sh
@@ -2,58 +2,80 @@
 
 cd ./resources
 
-echo -e "\n$(date +"%H:%M:%S"):\t$(soffice.bin --version)" >> test_results.txt
+echo -e "\n---------------------------------------------------------------------------------------------------------------" >> test_results.txt
+echo -e "$(date +"%H:%M:%S"):\t$(soffice.bin --version)" >> test_results.txt
 
-NO_OF_DOCUMENTS=100;
+NO_OF_DOCUMENTS=50;
 NO_OF_EXECUTIONS=10;
-for execution in $(seq 1 $NO_OF_EXECUTIONS); do
+FILENAMES=(\
+    'hello' \
+    'ODFAG' \
+    'OpenDocument' \
+    );
 
-    echo "Create the documents we want to use for testing."
-    for i in $(seq 1 $NO_OF_DOCUMENTS); do
-        cp input/hello.odt input/hello_$i.odt
+for ((f=0; f<${#FILENAMES[@]}; f++)); do
+    filename="${FILENAMES[$f]}";
+    filesize=$(stat -c%s "input/${filename}.odt")
+
+    declare -A durations;
+    for execution in $(seq 1 $NO_OF_EXECUTIONS); do
+
+        echo "Create the documents we want to use for testing."
+        for i in $(seq 1 $NO_OF_DOCUMENTS); do
+            cp input/${filename}.odt input/${filename}_$i.odt
+        done
+
+        echo "Create control file.";
+        soffice.bin \
+            --headless \
+            --nocrashreport \
+            --nodefault \
+            --nofirststartwizard \
+            --nolockcheck \
+            --nologo \
+            --norestore \
+            --invisible \
+            --convert-to pdf \
+            --outdir "result" input/${filename}.odt
+
+    #    if [ ! -e /vagrant_data/resources/result/${filename}.pdf ]; then
+    #        echo "$(date +"%H:%M:%S"): Document conversion failed." >> test_results_1.txt
+    #        exit 1;
+    #    fi
+
+        echo "Start document conversion."
+        startTime=$(date +%s);
+        for i in $(seq 1 $NO_OF_DOCUMENTS); do
+
+           soffice.bin \
+            --headless \
+            --nocrashreport \
+            --nodefault \
+            --nofirststartwizard \
+            --nolockcheck \
+            --nologo \
+            --norestore \
+            --invisible \
+            --convert-to pdf \
+             --outdir "result" input/${filename}_$i.odt
+        done
+        endTime=$(date +%s);
+        duration=$((endTime-startTime));
+        durations[$((execution-1))]=$duration;
+        echo -e "$(date +"%H:%M:%S"):\tExecution:\t[${execution}]\tNO_OF_DOCUMENTS:\t[${NO_OF_DOCUMENTS}]\tDocument size:\t[${filesize}]Kb\tTime:\t[${duration}]\tseconds." >> test_results.txt
+
+        echo "Clean test data."
+        for i in $(seq 1 $NO_OF_DOCUMENTS); do
+            rm -f input/${filename}_${i}.odt result/${filename}*.pdf
+        done
     done
 
-    echo "Create control file.";
-    soffice.bin \
-        --headless \
-        --nocrashreport \
-        --nodefault \
-        --nofirststartwizard \
-        --nolockcheck \
-        --nologo \
-        --norestore \
-        --invisible \
-        --convert-to pdf \
-        --outdir "result" input/hello.odt
-
-#    if [ ! -e /vagrant_data/resources/result/hello.pdf ]; then
-#        echo "$(date +"%H:%M:%S"): Document conversion failed." >> test_results.txt
-#        exit 1;
-#    fi
-
-    echo "Start document conversion."
-    startTime=$(date +%s);
-    for i in $(seq 1 $NO_OF_DOCUMENTS); do
-
-       soffice.bin \
-        --headless \
-        --nocrashreport \
-        --nodefault \
-        --nofirststartwizard \
-        --nolockcheck \
-        --nologo \
-        --norestore \
-        --invisible \
-        --convert-to pdf \
-         --outdir "result" input/hello_$i.odt
-    done
-    endTime=$(date +%s);
-    duration=$((endTime-startTime));
-    echo -e "$(date +"%H:%M:%S"): Execution:\t[${execution}]\tNO_OF_DOCUMENTS:\t[${NO_OF_DOCUMENTS}]\tTime:\t[${duration}]\tseconds." >> test_results.txt
-
-    echo "Clean test data."
-    for i in $(seq 1 $NO_OF_DOCUMENTS); do
-        rm -f input/hello_$i.odt result/hello*.pdf
-    done
-
+    average=$(
+       echo "${durations[*]}" | awk '{len=split($0, a, " ")}END{for(i in a) sum+=a[i]}END{print sum/len}'
+    );
+    standardDeviation=$(
+        echo "${durations[*]}" | awk '{len=split($0, a, " ")}END{for(i in a) {sum+=a[i]; sumsq+=a[i]*a[i]}}END{print (sumsq/len-(sum/len)^2)^0.5}'
+    )
+    echo -e "$(date +"%H:%M:%S"):\tDocument size:\t[${filesize}]Kb\tAverage duration:\t[${average}]\tseconds\tStandard Deviation:\t${standardDeviation}\t%\n"  >> test_results.txt
 done
+echo "---------------------------------------------------------------------------------------------------------------" >> test_results.txt
